9672626175f97a7db427995a042e2e00c2656a34
Svetlana <svetik.savina30@gmail.com>
2021-09-26 14:05:47 +0300

    home_work_lesson10

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function (){
+    $('.mobile_menu').on('click', function () {
+        $('.menu').slideToggle();
+    })
 
-});
+    const swiper = new Swiper('.gallery', {
+        loop: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 4,
+        spaceBetween: 30,
+        speed: 400
+      
+    });
+
+})
