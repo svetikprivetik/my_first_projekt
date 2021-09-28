dc20de34917726065573a1414751601afe812100
Svetlana <svetik.savina30@gmail.com>
2021-09-28 22:26:31 +0300

    services__about us__responsiv

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
-$(document).ready(function (){
-    $('.mobile_menu').on('click', function () {
+$(document).ready(function () {
+    $('.mobile-menu').on('click', function () {
         $('.menu').slideToggle();
     })
 
     const swiper = new Swiper('.gallery', {
         loop: true,
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
         slidesPerView: 4,
         spaceBetween: 30,
         speed: 400
-      
     });
-
 })
